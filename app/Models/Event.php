7fb32d5f2253f988c699c324fcc0972a9dad01c2
AsmaLad <asma.ladhari40@gmail.com>
2023-06-11 11:38:48 +0100

    change eventid to uuid done !

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Event extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description', 'date'];
 
-    
+    protected $primaryKey = 'uuid';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::creating(function (Model $model) {
+            $model->{$model->getKeyName()} = Uuid::uuid4()->toString();
+        });
+    }
 }
--- database/migrations/2023_06_11_101456_create_events_table.php
+++ database/migrations/2023_06_11_101456_create_events_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('uuid')->primary();
             $table->string('title');
             $table->text('description');
             $table->date('date');
